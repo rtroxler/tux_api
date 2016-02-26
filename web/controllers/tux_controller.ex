1634b329ca199ef9311e47347dbf64ab30437cdd
Ryan Troxler <rtroxler@rednovalabs.com>
2016-02-26 01:47:59 -0600

    Hook up national rates!

--- web/controllers/tux_controller.ex
+++ web/controllers/tux_controller.ex
@@ -6,7 +6,6 @@ defmodule TuxApi.TuxController do
   def process_rates(conn, %{"unit_dimensions" => unit_dimensions, "city_name" => city_name}) do
     [unit_length, unit_width] = split_unit_dimensions(unit_dimensions)
 
-    data =
     data = %{
       id: 1,
       type: "rate-datas",
@@ -16,9 +15,14 @@ defmodule TuxApi.TuxController do
     render conn, "process.json", data: [ data ]
   end
 
-  def process_all_rates(conn, _) do
-    data = %{ things: "hi" }
-    render conn, "process.json", data: data
+  def process_all_rates(conn, %{"unit_dimensions" => unit_dimensions}) do
+    [unit_length, unit_width] = split_unit_dimensions(unit_dimensions)
+    data = %{
+      id: 1,
+      type: "national-rate-datas",
+      attributes: Tux.process(unit_length, unit_width)
+    }
+    render conn, "process.json", data: [ data ]
   end
 
   def find_cities(conn, %{"unit_dimensions" => unit_dimensions}) do
